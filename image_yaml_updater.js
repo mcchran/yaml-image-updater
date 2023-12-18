bee1eabadfed9c5c81f1b604ad7930cac2139229
Christos O. Andrikos <mcchran@gmail.com>
2023-12-18 16:49:59 +0200

    initial commit

---
+++ image_yaml_updater.js
@@ -0,0 +1,68 @@
+const fs = require('fs');
+const yaml = require('js-yaml');
+
+function updateImageInYAML(filePath, dispatchedKey, dispatchedEvent) {
+  const fileContents = fs.readFileSync(filePath, 'utf8');
+  try {
+    const yamlData = yaml.load(fileContents);
+    if (yamlData && typeof yamlData === 'object') {
+      updateNestedImage(yamlData, dispatchedKey, dispatchedEvent);
+      const updatedYAML = yaml.dump(yamlData);
+      fs.writeFileSync(filePath, updatedYAML, 'utf8');
+      console.log(`Updated YAML file: ${filePath}`);
+    } else {
+      console.error('Invalid YAML structure. Root should be an object.');
+    }
+  } catch (error) {
+    console.error(error);
+    console.error(`Error parsing YAML file: ${filePath}`);
+  }
+}
+
+function updateNestedImage(yamlData, dispatchedKey, dispatchedEvent) {  
+  if (is_scanable(yamlData[dispatchedKey]) && has_image(yamlData[dispatchedKey])) {
+    updateImageValues(yamlData[dispatchedKey].image, dispatchedEvent);
+  }
+  Object.keys(yamlData).forEach((key) => {
+    console.log(key)
+    // need to evaluate if yamlData[key] is_scanable otherwise it will iterate over primitive types ... 
+    if (is_scanable(yamlData[key])) {
+      console.log(key)
+      updateNestedImage(yamlData[key], dispatchedKey, dispatchedEvent);
+    }
+  });
+}
+
+function is_scanable(yamlData) {
+  return typeof yamlData === "object" && yamlData != null ? true : false;
+}
+
+function has_image(yamlData) {
+  return yamlData.image ? true : false;
+}
+
+function updateImageValues(image, dispatchedEvent) {
+  if (dispatchedEvent.registry) {
+    image.registry = dispatchedEvent.registry;
+  }
+  if (dispatchedEvent.repository) {
+    image.repository = dispatchedEvent.repository;
+  }
+  if (dispatchedEvent.tag) {
+    image.tag = dispatchedEvent.tag;
+  }
+}
+
+
+module.exports = updateImageInYAML;
+
+// Example usage
+// const filePath = 'test-yamls/values.yaml';
+// const dispatchedKey = 'metrics';
+// const dispatchedEvent = {
+//   registry: 'newfoo',
+//   repository: 'repository',
+//   tag: 'v2',
+// };
+
+// updateImageInYAML(filePath, dispatchedKey, dispatchedEvent);
---
+++ index.js
@@ -0,0 +1,41 @@
+const glob = require('glob');
+const core = require("@actions/core");
+const updateImageInYAML = require('./image_yaml_updater');
+
+function getInputs() { 
+    const targetPath = core.getInput("target-path");
+    const dispatchedPayload = core.getInput("dispatched-payload");
+    return {
+        targetPath,
+        dispatchedPayload,
+    }; 
+}
+
+// imageDataByKey = {
+//     "service-name": {
+//         "image": "image-name",
+//         "tag": "image-tag",
+//         "registry": "image-registry",
+//         "repository": "image-repository",
+//     }
+function updateYamls(directoryPath, imageDataByKey) {
+    const filePaths = glob.sync(`${directoryPath}/**/values*.yaml`);
+    for (var file of filePaths) {
+        for (var serviceName in imageDataByKey) {
+            console.log(serviceName);
+            const imageInfo = imageDataByKey[serviceName];
+            console.log(imageInfo)
+            updateImageInYAML(file, serviceName, imageInfo);
+        }
+    }
+}
+
+async function main(){
+    const inputs = getInputs();
+    const yaml_root_dir = inputs.targetPath;
+    const dispatchedPayload = JSON.parse(inputs.dispatchedPayload);
+    console.log(dispatchedPayload)
+    updateYamls(yaml_root_dir, dispatchedPayload);
+}
+
+main();
